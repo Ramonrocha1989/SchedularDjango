0ad5d21b405ed0f7a127f79c31a9a7a5d9d24fe4
Ramon Rocha <ramon.rocha@compasso.com.br>
2024-12-30 09:34:45 -0300

    feat: configuring display admin

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id','first_name', 'last_name', 'phone', 'email', 'created_date',
+    ordering = 'id',
+    list_filter = 'created_date',
+    search_fields = 'id','first_name', 'last_name', 'phone', 'email',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
