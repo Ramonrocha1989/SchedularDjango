5997025331ac28e277e6de855f4dfed1b7288a65
Ramon Rocha <ramon.rocha@compasso.com.br>
2025-02-07 09:41:48 -0300

    feat: create initial form

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,23 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+
+from contact.models import Contact
+
+
+def create(request):
+    if request.method == 'POST':
+        
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        
+
+    context = {
+
+    }
+    
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
