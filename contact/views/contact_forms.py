792df0867d9542b423689015fdb85a18333e8da9
Ramon Rocha <ramon.rocha@compasso.com.br>
2025-02-11 12:01:58 -0300

    feat: initial form configuration

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,21 +1,23 @@
-from django.core.paginator import Paginator
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import render
 
-from contact.models import Contact
-
-
+from contact.forms import ContactForm
+    
 def create(request):
     if request.method == 'POST':
-        
-        print(request.POST.get('first_name'))
-        print(request.POST.get('last_name'))
-        
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
-
+        'form': ContactForm()
     }
-    
+
     return render(
         request,
         'contact/create.html',
