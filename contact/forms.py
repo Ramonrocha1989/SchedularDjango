f961e6c17572b01be12a75e3a827c56a3f351fea
Ramon Rocha <ramon.rocha@compasso.com.br>
2025-02-12 08:58:28 -0300

    feat: add create and update

--- contact/forms.py
+++ contact/forms.py
@@ -5,27 +5,53 @@ from . import models
 
 class ContactForm(forms.ModelForm):
     
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'write here',
+            }
+        ),
+        label='First Name',
+        help_text='Help text for your user',
+    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
         )
         
+    
     def clean(self):
-        # cleaned_data = self.cleaned_data
-        
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'First name cannot be the same as the second',
                 code='invalid'
             )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+            
         return super().clean()
+    
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'add_error',
+                    code='invalid'
+                )
+            )
+            
+        return first_name
