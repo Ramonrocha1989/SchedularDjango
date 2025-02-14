aa4fb8e7694e55a5bde3a59d9c8de4bec67a22a1
Ramon Rocha <ramon.rocha@compasso.com.br>
2025-02-14 09:11:34 -0300

    feat: add picture in the forms

--- contact/forms.py
+++ contact/forms.py
@@ -5,24 +5,20 @@ from . import models
 
 class ContactForm(forms.ModelForm):
     
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class': 'classe-a classe-b',
-                'placeholder': 'write here',
+                'accept': 'image/*',
             }
-        ),
-        label='First Name',
-        help_text='Help text for your user',
+        )
     )
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
+        
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture'
         )
         
     
